c793e872e0f8c432a779ba4ff89f79b23b421887
redmanuel1 <wilfred.a.manuel@gmail.com>
2024-10-15 00:28:09 +0800

    deployment 3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: false
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAor0vsOYA3yiXEFSNL_VyftuTZJTk4a60",
+    authDomain: "inventoryapp-2162b.firebaseapp.com",
+    projectId: "inventoryapp-2162b",
+    storageBucket: "inventoryapp-2162b.appspot.com",
+    messagingSenderId: "386387705752",
+    appId: "1:386387705752:web:325a619af00ebe2f8cf5ed",
+    measurementId: "G-3S6K26FVBL"
+  }
 };

da09ee73a4961f772664a764e0953c8280852eb0
redmanuel1 <wilfred.a.manuel@gmail.com>
2024-10-14 16:31:48 +0800

    accountant-confirmation

--- src/app/pages/student/transactions/order-details/order-details.component.ts
+++ src/app/pages/student/transactions/order-details/order-details.component.ts
@@ -216,9 +216,9 @@ export class OrderDetailsComponent implements OnInit {
     });
     this.orderProgress.push({
       title: "3. Awaiting for Payment Confirmation",
-      date: null,
+      date: this.transaction.statusUpdates?.[this.transaction.statusUpdates.length - 1]?.dateUpdated ?? null,
     });
-    this.orderProgress.push({ title: "4. Item for Pickup", date: null });
+    this.orderProgress.push({ title: "4. Item for Pickup", date: this.transaction.confirmedDate ?? null });
     this.orderProgress.push({ title: "5. Transaction Complete", date: null });
   }
   setSelectedIndex(index: number) {

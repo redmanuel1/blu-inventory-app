9e9328b9616a8a55699923ca482f0bb20b1fc587
mattsayco <mattsayco@gmail.com>
2024-10-15 17:45:55 +0800

    Hide the searchbar and Added deactivate function for managing users

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,95 +1,110 @@
-import { CustodianLayoutModule } from './layouts/custodian-layout/custodian-layout.module';
-import { NgModule } from '@angular/core';
-import { CommonModule, } from '@angular/common';
-import { BrowserModule  } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
+import { CustodianLayoutModule } from "./layouts/custodian-layout/custodian-layout.module";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
+import { Routes, RouterModule } from "@angular/router";
 
-import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
-import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
-import { AuthGuard } from './guards/auth.guard';
-import { StudentLayoutComponent } from './layouts/student-layout/student-layout.component';
-import { LoginGuard } from './guards/login.guard';
-import { ItemComponent } from './components/cards/item/item.component';
-import { CustodianLayoutComponent } from './layouts/custodian-layout/custodian-layout.component';
-import { CheckoutComponent } from './pages/student/checkout/checkout.component';
-import { AccountantLayoutComponent } from './layouts/accountant-layout/accountant-layout.component';
+import { AdminLayoutComponent } from "./layouts/admin-layout/admin-layout.component";
+import { AuthLayoutComponent } from "./layouts/auth-layout/auth-layout.component";
+import { AuthGuard } from "./guards/auth.guard";
+import { StudentLayoutComponent } from "./layouts/student-layout/student-layout.component";
+import { LoginGuard } from "./guards/login.guard";
+import { ItemComponent } from "./components/cards/item/item.component";
+import { CustodianLayoutComponent } from "./layouts/custodian-layout/custodian-layout.component";
+import { CheckoutComponent } from "./pages/student/checkout/checkout.component";
+import { AccountantLayoutComponent } from "./layouts/accountant-layout/accountant-layout.component";
 
-const routes: Routes =[
+const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'auth/login',
-    pathMatch: 'full',
-  }, {
-    path: 'admin',
+    path: "",
+    redirectTo: "auth/login",
+    pathMatch: "full",
+  },
+  {
+    path: "admin",
     component: AdminLayoutComponent,
     canActivate: [AuthGuard],
     children: [
       {
-        path: '',
-        loadChildren: () => import('src/app/layouts/admin-layout/admin-layout.module').then(m => m.AdminLayoutModule)
-      }
-    ]
+        path: "",
+        loadChildren: () =>
+          import("src/app/layouts/admin-layout/admin-layout.module").then(
+            (m) => m.AdminLayoutModule
+          ),
+      },
+    ],
   },
   {
-    path: 'student',
+    path: "student",
     component: StudentLayoutComponent,
     canActivate: [AuthGuard], // Add role guard to check if the user is a student
     children: [
       {
-        path: '',
-        loadChildren: () => import('./layouts/student-layout/student-layout.module').then(m => m.StudentLayoutModule)
-      }
-    ]
-  }, 
+        path: "",
+        loadChildren: () =>
+          import("./layouts/student-layout/student-layout.module").then(
+            (m) => m.StudentLayoutModule
+          ),
+      },
+    ],
+  },
   {
-    path: 'custodian',
+    path: "custodian",
     component: CustodianLayoutComponent,
     canActivate: [AuthGuard], // Add role guard to check if the user is a student
     children: [
       {
-        path: '',
-        loadChildren: () => import('./layouts/custodian-layout/custodian-layout.module').then(m => m.CustodianLayoutModule)
-      }
-    ]
+        path: "",
+        loadChildren: () =>
+          import("./layouts/custodian-layout/custodian-layout.module").then(
+            (m) => m.CustodianLayoutModule
+          ),
+      },
+    ],
   },
   {
-    path: 'accountant',
+    path: "accountant",
     component: AccountantLayoutComponent,
     canActivate: [AuthGuard], // Add role guard to check if the user is a student
     children: [
       {
-        path: '',
-        loadChildren: () => import('./layouts/accountant-layout/accountant-layout.module').then(m => m.AccountantLayoutModule)
-      }
-    ]
-  }, 
+        path: "",
+        loadChildren: () =>
+          import("./layouts/accountant-layout/accountant-layout.module").then(
+            (m) => m.AccountantLayoutModule
+          ),
+      },
+    ],
+  },
   {
-    path: 'auth',
+    path: "auth",
     component: AuthLayoutComponent,
     canActivate: [LoginGuard],
     children: [
       {
-        path: '',
-        loadChildren: () => import('src/app/layouts/auth-layout/auth-layout.module').then(m => m.AuthLayoutModule)
+        path: "",
+        loadChildren: () =>
+          import("src/app/layouts/auth-layout/auth-layout.module").then(
+            (m) => m.AuthLayoutModule
+          ),
       },
-    ]
+    ],
   },
 
   {
-    path: '**',
-    redirectTo: '/dashboard'
-  }
+    path: "**",
+    redirectTo: "/dashboard",
+  },
 ];
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserModule,
-    RouterModule.forRoot(routes,{
-      useHash: false
-    })
+    RouterModule.forRoot(routes, {
+      useHash: false,
+    }),
   ],
-  exports: [
-  ],
+  exports: [],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -1,18 +1,20 @@
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { ClipboardModule } from 'ngx-clipboard';
+import { ClipboardModule } from "ngx-clipboard";
 
-import { AdminLayoutRoutes } from './admin-layout.routing';
-import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
-import { IconsComponent } from '../../pages/icons/icons.component';
-import { MapsComponent } from '../../pages/maps/maps.component';
-import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
-import { TablesComponent } from '../../pages/tables/tables.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ComponentsModule } from 'src/app/components/components.module';
+import { AdminLayoutRoutes } from "./admin-layout.routing";
+import { DashboardComponent } from "../../pages/dashboard/dashboard.component";
+import { IconsComponent } from "../../pages/icons/icons.component";
+import { MapsComponent } from "../../pages/maps/maps.component";
+import { UserProfileComponent } from "../../pages/user-profile/user-profile.component";
+import { TablesComponent } from "../../pages/tables/tables.component";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { ComponentsModule } from "src/app/components/components.module";
+import { ManageUsersComponent } from "src/app/pages/manage-users/manage-users.component";
 // import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
@@ -23,15 +25,16 @@ import { ComponentsModule } from 'src/app/components/components.module';
     HttpClientModule,
     NgbModule,
     ClipboardModule,
-    ComponentsModule
+    ComponentsModule,
   ],
   declarations: [
     DashboardComponent,
     UserProfileComponent,
     TablesComponent,
     IconsComponent,
-    MapsComponent
-  ]
+    MapsComponent,
+    ManageUsersComponent,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-
 export class AdminLayoutModule {}
